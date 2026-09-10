7d8ba656ccd1602a688fed79a3e892f28e7e61b1
Akbar <rifaiakbarnot@gmail.com>
2026-09-10 22:13:18 +0700

    Menambahkan beberapa latihan array

---
+++ array/array1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int angka[5];
+    int total = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Masukkan angka ke-%d: ", i + 1);
+        scanf("%d", &angka[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        total += angka[i];
+    }
+    float rataRata = (float)total / 5;
+    printf("Jumlah dan rata-rata kelima angka adalah %d dan %.2f \n", total, rataRata);
+
+    return 0;
+}
